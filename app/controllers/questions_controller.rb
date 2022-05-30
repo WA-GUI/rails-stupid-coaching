a31402fc4952916ed6f9282ed034f1017b211f41
Guillaume Genet <wagui.genet@gmail.com>
2022-05-30 13:32:43 +0200

    first app

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    q = params[:answer]
+
+    if q == 'I am going to work'
+      @answer = 'Great'
+    elsif q.chars.last == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
